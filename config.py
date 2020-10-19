bc018ec3a09d1ddf7b8086af9859cf92a97134ea
Adam <abestic@mac.com>
2020-10-19 16:52:08 -0400

    Clean up (#1)
    * removed old, commented out routes
    
    * added clarifying comments
    
    * no forms in main blueprint
    
    * removed unneeded comments
    
    * added clarifying comments

--- config.py
+++ config.py
@@ -5,10 +5,3 @@ class Config(object):
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-
-# TO-DO:
-# - Set SECRET_KEY as an OCP secret that populates an env variable
-# - Set DATABASE_URL as an OCP secret that populates an env variable
-# - Determine if config.py needs to be treated as a config map when deploying to OCP...
--- qanda.py
+++ qanda.py
@@ -3,6 +3,7 @@ from app.models import User, Topic, Question, Answer
 
 app = create_app()
 
+# Provide python interpreter shell with this app's context
 @app.shell_context_processor
 def make_shell_context():
   return {'db': db, 'User': User, 'Topic': Topic, 'Question': Question, 'Answer': Answer}
