037706351111ed9e69efbebc73afebec2cc5cfc7
abestic <abestic@mac.com>
2020-10-20 18:55:10 -0400

    contextualize the widget for a logged-out state

--- app/main/routes.py
+++ app/main/routes.py
@@ -9,8 +9,12 @@ from werkzeug.urls import url_parse
 @bp.route('/')
 @bp.route('/index')
 def index():
-  my_question_count = Question.query.filter_by(user_id=current_user.id).count()
-  my_answer_count = Answer.query.filter_by(user_id=current_user.id).count()
   popular_questions = Question.query.order_by(Question.total_votes.desc()).limit(10)
   recent_questions = Question.query.order_by(Question.id.desc()).limit(10)
-  return render_template('index.html', title='Home', my_question_count=my_question_count, my_answer_count=my_answer_count, popular_questions=popular_questions, recent_questions=recent_questions)
+  if current_user.is_authenticated:
+    my_question_count = Question.query.filter_by(user_id=current_user.id).count()
+    my_answer_count = Answer.query.filter_by(user_id=current_user.id).count()
+    return render_template('index.html', title='Home', my_question_count=my_question_count, my_answer_count=my_answer_count, popular_questions=popular_questions, recent_questions=recent_questions)
+  else:
+    return render_template('index.html', title='Home', popular_questions=popular_questions, recent_questions=recent_questions)
+  
