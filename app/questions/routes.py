3613721d7229fefc1b9703616e81fb66f8975cb6
Adam <abestic@mac.com>
2020-10-20 00:11:42 -0400

    Auto populate topic (#4)
    * added topic id url argument for new question
    * populate specific topic in dropdown when url arg is present
    * removed unneeded commented lines

--- app/questions/routes.py
+++ app/questions/routes.py
@@ -4,7 +4,7 @@ from app.questions.forms import QuestionForm
 from app.answers.forms import QAForm
 from app.models import Question, Topic, User
 
-from flask import render_template, flash, redirect, url_for, jsonify
+from flask import render_template, flash, redirect, url_for, jsonify, request
 from flask_login import current_user, login_user, logout_user, login_required
 
 
@@ -31,7 +31,12 @@ def create():
 @bp.route('/new', methods=['GET'])
 def new():
   form = QuestionForm()
-  form.topic_id.choices = [(t.id, t.name) for t in Topic.query.order_by('name')]
+  topic = request.args.get('topic') # argument passed in url by New Question button on Topic Detail page
+  if topic: # if topic argument is present, populate new question's topic dropdown with this topic
+    t = Topic.query.filter_by(id=topic).first()
+    form.topic_id.choices = [(t.id, t.name)]
+  else: # if topic argument is not present, populate new question's topic dropdown wtith all topic ids
+    form.topic_id.choices = [(t.id, t.name) for t in Topic.query.order_by('name')]
   backroute = '/questions/create'
   verb = 'POST'
   return render_template('question_form.html', title='Ask Question', form=form, backroute=backroute, verb=verb)
@@ -58,6 +63,7 @@ def edit(id):
   question = Question.query.filter_by(id=id).first()
   form = QuestionForm(obj=question) # pre-populate the form with the representation of the current record in the DB
   form.topic_id.choices = [(t.id, t.name) for t in Topic.query.order_by('name')]
+  form.topic_id.default = [(question.topic.id, question.topic.name)] # set the default value of the topic dropdown to the question's current topic
   backroute = '/questions/' + id
   verb = 'POST'
   return render_template('question_form.html', title='Edit Question', form=form, backroute=backroute, verb=verb)
