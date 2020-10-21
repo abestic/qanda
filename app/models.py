a72b3c2fa25a25eff1f500a197b0718cfc3a0ceb
abestic <abestic@mac.com>
2020-10-21 19:12:08 -0400

    Added SearchableMixin to Question model

--- app/models.py
+++ app/models.py
@@ -6,79 +6,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
-# User Loader
-@login.user_loader
-def user_loader(id):
-  return User.query.get(int(id))
-
-# Users Table
-# UserMixin provides is_authenticated, is_active, is_anonymous, and get_id() methods needed by Flask-Login
-class User(UserMixin, db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  username = db.Column(db.String(64), index=True, unique=True)
-  email = db.Column(db.String(120), index=True, unique=True)
-  password_hash = db.Column(db.String(128))
-  questions = db.relationship('Question', backref='author', lazy='dynamic')
-  answers = db.relationship('Answer', backref='author', lazy='dynamic')
-
-  # format the object output
-  def __repr__(self):
-    return '<User {}>'.format(self.username)
-
-  def set_password(self, password):
-    self.password_hash = generate_password_hash(password)
-
-  def check_password(self, password):
-    return check_password_hash(self.password_hash, password)
-
-
-# Topics Table
-class Topic(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(50), index=True, unique=True)
-  description = db.Column(db.String(255), index=True)
-  questions = db.relationship('Question', backref='topic', lazy='dynamic')
-  __searchable__ = ['name', 'description']
-
-  # format the object output
-  def __repr__(self):
-    return '<Topic {}>'.format(self.name)
-
-
-# Questions Table
-class Question(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  text = db.Column(db.String(255), index=True, unique=True)
-  description = db.Column(db.Text, index=True)
-  up_votes = db.Column(db.Integer, index=True)
-  down_votes = db.Column(db.Integer, index=True)
-  total_votes = db.Column(db.Integer, index=True)
-  topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-  answers = db.relationship('Answer', backref='question', lazy='dynamic')
-  __searchable__ = ['text', 'description']
-
-  # format the object output
-  def __repr__(self):
-    return '<Question {}>'.format(self.text)
-
-
-# Answers Table
-class Answer(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  text = db.Column(db.Text, index=True)
-  up_votes = db.Column(db.Integer, index=True)
-  down_votes = db.Column(db.Integer, index=True)
-  total_votes = db.Column(db.Integer, index=True)
-  question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-  __searchable__ = ['text']
-  
-  # format the object output
-  def __repr__(self):
-    return '<Answer {}>'.format(self.text)
-
-
 # Searchable Mixin
 class SearchableMixin(object):
   @classmethod
@@ -119,3 +46,77 @@ class SearchableMixin(object):
 
 db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
 db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
+
+
+# User Loader
+@login.user_loader
+def user_loader(id):
+  return User.query.get(int(id))
+
+
+# Users Table
+# UserMixin provides is_authenticated, is_active, is_anonymous, and get_id() methods needed by Flask-Login
+class User(UserMixin, db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(64), index=True, unique=True)
+  email = db.Column(db.String(120), index=True, unique=True)
+  password_hash = db.Column(db.String(128))
+  questions = db.relationship('Question', backref='author', lazy='dynamic')
+  answers = db.relationship('Answer', backref='author', lazy='dynamic')
+
+  # format the object output
+  def __repr__(self):
+    return '<User {}>'.format(self.username)
+
+  def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
+
+  def check_password(self, password):
+    return check_password_hash(self.password_hash, password)
+
+
+# Topics Table
+class Topic(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(50), index=True, unique=True)
+  description = db.Column(db.String(255), index=True)
+  questions = db.relationship('Question', backref='topic', lazy='dynamic')
+  __searchable__ = ['name', 'description']
+
+  # format the object output
+  def __repr__(self):
+    return '<Topic {}>'.format(self.name)
+
+
+# Questions Table
+class Question(SearchableMixin, db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  text = db.Column(db.String(255), index=True, unique=True)
+  description = db.Column(db.Text, index=True)
+  up_votes = db.Column(db.Integer, index=True)
+  down_votes = db.Column(db.Integer, index=True)
+  total_votes = db.Column(db.Integer, index=True)
+  topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  answers = db.relationship('Answer', backref='question', lazy='dynamic')
+  __searchable__ = ['text', 'description']
+
+  # format the object output
+  def __repr__(self):
+    return '<Question {}>'.format(self.text)
+
+
+# Answers Table
+class Answer(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  text = db.Column(db.Text, index=True)
+  up_votes = db.Column(db.Integer, index=True)
+  down_votes = db.Column(db.Integer, index=True)
+  total_votes = db.Column(db.Integer, index=True)
+  question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  __searchable__ = ['text']
+  
+  # format the object output
+  def __repr__(self):
+    return '<Answer {}>'.format(self.text)
