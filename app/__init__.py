380968a33466b452066628297ad6b6285df02fc5
abestic <abestic@mac.com>
2020-10-21 14:20:11 -0400

    Added Elasticsearch attribute to app instance

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,9 @@ def create_app(config_class=Config):
   app = Flask(__name__)
   app.config.from_object(config_class)
 
+  app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
+    if app.config['ELASTICSEARCH_URL'] else None
+
   db.init_app(app)
   migrate.init_app(app, db)
   login.init_app(app)
